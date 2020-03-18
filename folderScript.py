54f2123ae85c3af93de5919ffb5781d3c8c24599
OmarMch <mhcomar28@gmail.com>
2020-03-18 12:17:40 -0500

    Added python script for folder loop creation

---
+++ folderScript.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+
+# detect the current directory and print it
+# _path_dir = os.getcwd()
+
+pathname = 1
+
+# define the name of the directory to be created(replace "path" with own)
+path_dir = "/path/Chapter_" + str(pathname)
+
+# folder access rights 
+access_rights = 0o777
+
+
+while pathname < 14:
+    if not os.path.exists(path_dir):
+        os.makedirs(path_dir, access_rights)
+    pathname += 1
+    # replace "path" as well.
+    path_dir = "/path/Chapter_" + str(pathname)
+    print("Successfuly created the directory %s" % pathname)
+
+
+
